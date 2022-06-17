27ab06f941d9705f0ea6f6a3270ab2a122947146
Tony Hawkings <tonyhawkingsexample@example.com>
2022-06-17 15:31:58 +0900

    first commit

---
+++ DP/丸太の切断.py
@@ -0,0 +1,10 @@
+def maxLogProfit(priceList,n):
+    DP = [0] + priceList
+
+    j = 0
+    for i in range(1, len(DP)):
+        while i > j:
+            DP[i] = max(DP[i], DP[i - j] + DP[j])
+            j += 1
+        j = 0
+    return DP [-1]
